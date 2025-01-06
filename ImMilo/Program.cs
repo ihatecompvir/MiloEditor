bad1f012bf410cc993518087f70f270a64511ff1
Sulfrix <deucebroom@outlook.com>
2025-01-06 06:13:13 -0600

    add ImMilo

---
+++ ImMilo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ImMilo;
+
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
