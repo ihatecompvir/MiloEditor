56ea3dc2c6e15b53144536e939682625340fc4b7
Sulfrix <deucebroom@outlook.com>
2025-01-24 14:00:47 -0600

    Some more enum names

--- MiloLib/Utils/Endian/Endian.cs
+++ MiloLib/Utils/Endian/Endian.cs
@@ -1,11 +1,15 @@
 ﻿// originally taken from https://github.com/XboxChaos/Assembly, thanks for originally writing this code
 // license in Utils/Endian/LICENSE.md
 
+using MiloLib.Classes;
+
 namespace MiloLib.Utils
 {
     public enum Endian
     {
+        [Name("Big Endian (RB1 and later)")]
         BigEndian, // MSB -> LSB
+        [Name("Little Endian (GH2 and earlier)")]
         LittleEndian // LSB -> MSB
     }
 }
