f88a063fba4f662556ad1b3d153de91df9fd8eba
ihatecompvir <ihatecompvir@live.com>
2025-06-15 21:46:34 -0700

    Remove outdated, unnecessary tests

--- MiloLib.Tests/IOTests.cs
+++ MiloLib.Tests/IOTests.cs
@@ -7,40 +7,5 @@ using MiloLib.Utils;
 
 public class IOTests
 {
-    /// <summary>
-    /// Creates an RB3-versioned ObjectDir, writes it to a MemoryStream, reads it back, and compares some fields to ensure they were written and read correctly.
-    /// </summary>
-    [Fact]
-    public void TestRB3ObjectDirCreation()
-    {
-        ObjectDir objectDir = new ObjectDir(27);
-
-        MemoryStream stream = new MemoryStream();
-        EndianWriter writer = new EndianWriter(stream, Endian.BigEndian);
-
-        objectDir.objFields.type = "Test_Directory";
-        objectDir.proxyPath = "test_path.milo";
-
-        objectDir.Write(writer, false, new DirectoryMeta(), null);
-
-        MemoryStream stream2 = new MemoryStream();
-
-        stream.Position = 0;
-
-        stream.CopyTo(stream2);
-
-        stream2.Position = 0;
-
-        EndianReader reader = new EndianReader(stream2, Endian.BigEndian);
-
-        ObjectDir objectDir2 = new ObjectDir(27);
-        objectDir2.Read(reader, false, new DirectoryMeta(), new DirectoryMeta.Entry("ObjectDir", "test", objectDir2));
-
-        // compare the two fields we set in the ObjectDirs
-        Assert.Equal(objectDir.objFields.type.value, objectDir2.objFields.type.value);
-        Assert.Equal(objectDir.proxyPath.value, objectDir2.proxyPath.value);
-
-        // make sure the two MemoryStreams have the same size, meaning the same data was written and read
-        Assert.Equal(stream.Length, stream2.Length);
-    }
+    // TODO: Actually add tests lmao
 }
